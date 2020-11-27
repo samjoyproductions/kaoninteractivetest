4eafbf78b517175c25b824f8bc1b48f794e921a9
Samuel Joy <samjoy.productions@gmail.com>
2020-11-27 16:46:44 -0500

    update animations

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
 	
 	$("[data-js-page]").on("touchend", function(){
+
 		var goal_page = $(this).data("js-page");
-		$(".page").removeClass("active");
-		$(".page[data-page='"+goal_page+"']").addClass("active");
+		if (goal_page === "med" || goal_page === "gov") {
+			$(".o_backgroundImages").addClass(`o_backgroundImages--${goal_page}Active`);
+			$(".o_controls").addClass(`o_controls--${goal_page}Active`);
+		}
+		if (goal_page === "entry") {
+			$(".o_backgroundImages").attr('class', 'o_backgroundImages');
+			$(".o_controls").attr('class', 'o_controls');
+		}
 	})
 
 });
