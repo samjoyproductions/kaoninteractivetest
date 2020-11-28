ce0d5fef48aba74bab2f4d010bce324dc5c914c2
Samuel Joy <samjoy.productions@gmail.com>
2020-11-28 14:45:29 -0500

    finalized code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	
+	// handles transitions between screens
 	$("[data-js-page]").on("touchend", function(){
 
 		var goal_page = $(this).data("js-page");
+
+		
 		if (goal_page === "med" || goal_page === "gov") {
 			$(".o_backgroundImages").addClass(`o_backgroundImages--${goal_page}Active`);
 			$(".o_controls").addClass(`o_controls--${goal_page}Active`);
@@ -11,6 +14,21 @@ $(document).ready(function(){
 			$(".o_backgroundImages").attr('class', 'o_backgroundImages');
 			$(".o_controls").attr('class', 'o_controls');
 		}
-	})
+	});
+	
+	// handles open and close of menu on gov screen
+	$("[data-js-govmenu]").on("touchend", function(event){
+		var goal_action = $(this).data("js-govmenu");
+		
+		if (event.target.classList.contains("u_exploreMenu--preventClose")){
+			return;
+		} 
+		if (goal_action === "toggleon"){
+			$(".c_govControls").addClass('c_govControls--menuActive');
+		}
+		if (goal_action === "toggleoff"){
+			$(".c_govControls").attr('class', 'c_govControls');
+		}
+	});
 
 });
